57796171ca8131e4c048f2fece39179b0a424c3a
boudoual <alexandre.bourdouxhe@arhs-dev-be.com>
2021-02-25 16:36:23 +0100

    Implementing speciality entity and updated Vet entity with speciality relationship. Closes #29.

---
+++ pet-clinic-data/src/main/java/spring/training/personal/petclinic/model/Speciality.java
@@ -0,0 +1,13 @@
+package spring.training.personal.petclinic.model;
+
+public class Speciality extends BaseEntity {
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(final String description) {
+        this.description = description;
+    }
+}
