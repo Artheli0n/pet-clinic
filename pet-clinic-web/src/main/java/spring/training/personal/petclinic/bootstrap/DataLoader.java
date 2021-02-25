fa78a34db5e6f85cf2fde8085b6d6a73744ec736
boudoual <alexandre.bourdouxhe@arhs-dev-be.com>
2021-02-25 16:59:18 +0100

    Added PetTYpes to bootstrap. Closes #34.

--- pet-clinic-web/src/main/java/spring/training/personal/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/spring/training/personal/petclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package spring.training.personal.petclinic.bootstrap;
 
 import spring.training.personal.petclinic.model.Owner;
+import spring.training.personal.petclinic.model.PetType;
 import spring.training.personal.petclinic.model.Vet;
 import spring.training.personal.petclinic.services.OwnerService;
+import spring.training.personal.petclinic.services.PetTypeService;
 import spring.training.personal.petclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,15 +14,27 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
     public DataLoader(final OwnerService ownerService,
-                      final VetService vetService) {
+                      final VetService vetService,
+                      final PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(final String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
