7b8bb000595ecb4b373cf95474d8ad0fb9917626
boudoual <alexandre.bourdouxhe@arhs-dev-be.com>
2021-02-25 16:42:59 +0100

    Added Contact info to Owner Entity. Closes #30.

--- pet-clinic-data/src/main/java/spring/training/personal/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/spring/training/personal/petclinic/model/Owner.java
@@ -3,7 +3,11 @@ package spring.training.personal.petclinic.model;
 import java.util.Set;
 
 public class Owner extends Person {
+
     private Set<Pet> pets;
+    private String address;
+    private String city;
+    private String phoneNumber;
 
     public Set<Pet> getPets() {
         return pets;
@@ -11,5 +15,29 @@ public class Owner extends Person {
 
     public void setPets(final Set<Pet> pets) {
         this.pets = pets;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(final String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(final String city) {
+        this.city = city;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(final String phoneNumber) {
+        this.phoneNumber = phoneNumber;
     }
 }
