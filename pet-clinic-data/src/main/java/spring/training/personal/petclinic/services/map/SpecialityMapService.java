343f389f4a423d3dcfc0b32075012f0b6a2f985b
boudoual <alexandre.bourdouxhe@arhs-dev-be.com>
2021-02-26 10:03:53 +0100

    Implemented the Speciality Map Service. Closes #33.

---
+++ pet-clinic-data/src/main/java/spring/training/personal/petclinic/services/map/SpecialityMapService.java
@@ -0,0 +1,36 @@
+package spring.training.personal.petclinic.services.map;
+
+import spring.training.personal.petclinic.model.Speciality;
+import spring.training.personal.petclinic.services.SpecialityService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialityMapService extends AbstractMapService<Speciality, Long> implements SpecialityService {
+
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Speciality findById(final Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Speciality save(final Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(final Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(final Long id) {
+        super.deleteById(id);
+    }
+}
