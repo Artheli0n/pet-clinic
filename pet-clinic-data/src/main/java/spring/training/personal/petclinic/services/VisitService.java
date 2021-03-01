2737fa3436aeb4ccdd557e3a104a14e6d8689dbb
boudoual <alexandre.bourdouxhe@arhs-dev-be.com>
2021-03-01 15:08:31 +0100

    Implemented Visit Map Service. Closes #48

---
+++ pet-clinic-data/src/main/java/spring/training/personal/petclinic/services/VisitService.java
@@ -0,0 +1,7 @@
+package spring.training.personal.petclinic.services;
+
+import spring.training.personal.petclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/spring/training/personal/petclinic/services/map/VisitMapService.java
@@ -0,0 +1,43 @@
+package spring.training.personal.petclinic.services.map;
+
+import spring.training.personal.petclinic.model.Visit;
+import spring.training.personal.petclinic.services.VisitService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+@Profile({"map"})
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(final Long aLong) {
+        return super.findById(aLong);
+    }
+
+    @Override
+    public Visit save(final Visit visit) {
+
+        if (visit.getPet() == null || visit.getPet().getOwner() == null | visit.getPet().getId() == null || visit.getPet().getOwner().getId() == null) {
+            throw new RuntimeException("Invalid Visit Parameters");
+        }
+
+        return super.save(visit);
+    }
+
+    @Override
+    public void delete(final Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(final Long aLong) {
+        super.deleteById(aLong);
+    }
+}
