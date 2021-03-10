6ec05c0da28b40e9767112847adf4139285609e6
boudoual <alexandre.bourdouxhe@arhs-dev-be.com>
2021-03-10 11:26:05 +0100

    Added json endpoint. Closes #70

--- pet-clinic-web/src/main/java/spring/training/personal/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/spring/training/personal/petclinic/controllers/VetController.java
@@ -1,9 +1,14 @@
 package spring.training.personal.petclinic.controllers;
 
+import spring.training.personal.petclinic.model.Vet;
 import spring.training.personal.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -19,5 +24,12 @@ public class VetController {
         model.addAttribute("vets", vetService.findAll());
 
         return "vets/vetList";
+    }
+
+    // the @ResponseBody basically says "format the return value as a ResponseBody head"
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson() {
+
+        return vetService.findAll();
     }
 }
